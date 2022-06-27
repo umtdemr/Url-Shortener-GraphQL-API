c90cff26f97f10965ef65619c0dadeda042fc67f
umtdemr <umitde692@gmail.com>
2022-06-27 21:40:44 +0300

    added starting functions with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world, just checking if the app is working')
+import express from 'express';
+
+
+const app = express()
+
+
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.json({message: 'started'})
+})
+
+
+app.listen(3000, () => console.log('server is running at 3000'))
