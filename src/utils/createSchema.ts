232871a3efe0608ddf6b46e05a665b121c4c6a8e
umtdemr <umitde692@gmail.com>
2022-06-28 15:40:01 +0300

    removed validate option on creating schema for validation

--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -3,6 +3,5 @@ import { buildSchema } from 'type-graphql'
 export const createSchema = async () => {
   return await buildSchema({
     resolvers: [__dirname + '/../resolvers/*.ts'],
-    validate: false
   })
 }
